886910cf4961b879757f25b669ac0f370bacf78c
Junlang Chen <143589428+JunlangChenGT@users.noreply.github.com>
2023-10-31 03:15:26 -0500

    contacts collection finished in 75 minutes
    75:24

---
+++ app/imports/ui/components/ContactAdmin.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Image } from 'react-bootstrap';
+import Footer from './Footer';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+const ContactAdmin = ({ contact }) => (
+  <Card>
+    <Card.Header>
+      <Image src={contact.image} width={75} />
+    </Card.Header>
+    <Card.Title>{contact.firstName} {contact.lastName}</Card.Title>
+    <Card.Subtitle>{contact.address}</Card.Subtitle>
+    <Card.Body>
+      <Card.Text>{contact.description}</Card.Text>
+      <Footer classname="blockquote-footer">{contact.owner}</Footer>
+    </Card.Body>
+  </Card>
+);
+
+// Require a document to be passed to this component.
+ContactAdmin.propTypes = {
+  contact: PropTypes.shape({
+    firstName: PropTypes.string,
+    lastName: PropTypes.string,
+    address: PropTypes.string,
+    image: PropTypes.string,
+    description: PropTypes.string,
+    owner: PropTypes.string,
+    // _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default ContactAdmin;
