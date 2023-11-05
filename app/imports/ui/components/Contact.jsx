7659acbbee40bd651f87a3e47f1bb69e0e068487
Junlang Chen <143589428+JunlangChenGT@users.noreply.github.com>
2023-11-05 02:43:34 -0600

    notes-2 finished in 75 minutes
    i spent a whole hour debugging this thing by going frame by frame though the tutorial and it ended up being mongo being corrupted...
    didn't think of meteor reset until 70 minutes in
    i want my hour back

--- app/imports/ui/components/Contact.jsx
+++ app/imports/ui/components/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Card, Image } from 'react-bootstrap';
+import { Card, Image, ListGroup } from 'react-bootstrap';
+import Note from './Note';
+import AddNote from './AddNote';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const StuffItem = ({ contact }) => (
+const StuffItem = ({ contact, notes }) => (
   <Card>
     <Card.Header>
       <Image src={contact.image} width={75} />
@@ -13,6 +15,10 @@ const StuffItem = ({ contact }) => (
     <Card.Subtitle>{contact.address}</Card.Subtitle>
     <Card.Body>
       <Card.Text>{contact.description}</Card.Text>
+      <ListGroup variant="flush">
+        {notes.map((note) => <Note key={note._id} note={note} />)}
+      </ListGroup>
+      <AddNote owner={contact.owner} contactId={contact._id} />
       <Link to={`/edit/${contact._id}`}>Edit</Link>
     </Card.Body>
   </Card>
@@ -28,8 +34,16 @@ StuffItem.propTypes = {
     description: PropTypes.string,
     quantity: PropTypes.number,
     condition: PropTypes.string,
+    owner: PropTypes.string,
     _id: PropTypes.string,
   }).isRequired,
+  notes: PropTypes.arrayOf(PropTypes.shape({
+    note: PropTypes.string,
+    contactId: PropTypes.string,
+    owner: PropTypes.string,
+    createdAt: PropTypes.instanceOf(Date),
+    _id: PropTypes.string,
+  })).isRequired,
 };
 
 export default StuffItem;
